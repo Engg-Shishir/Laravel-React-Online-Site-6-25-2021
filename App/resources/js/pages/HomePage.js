abf7ab48733cd8c0a06d680ad9f607b4b3373a83
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2021-06-26 18:36:29 +0600

    Gt Home Features Data

--- App/resources/js/pages/HomePage.js
+++ App/resources/js/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React, { Component,Fragment } from 'react';
 import Menu from "../components/Menu";
 import HomeBanner from "../components/HomeBanner";
+import CourseFeature from "../components/CourseFeature";
 class HomePage extends Component {
     render() {
         return (
             <Fragment>
                 <Menu/>
                 <HomeBanner/>
+                <CourseFeature/>
 
             </Fragment>
         );
